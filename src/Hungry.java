773a80abbdf94f94039821ec655c5052e7bf9b6b
gih1 <ghj12222@gmail.com>
2023-06-05 19:01:00 +0900

    if

---
+++ src/Hungry.java
@@ -0,0 +1,11 @@
+public class Hungry {
+    int money = 8500;{
+        if (money >= 8000){
+            System.out.println("순대국 맛있어!");
+        }else if(money >= 5000){
+            System.out.println("스테이크 맛있어!");
+        }else{
+            System.out.println("배고프다.");
+        }
+    }
+}
