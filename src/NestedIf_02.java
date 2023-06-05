adf2c194beca8497af8d3a78687d030a6e7bc586
gih1 <ghj12222@gmail.com>
2023-06-05 19:07:50 +0900

    if&&

---
+++ src/NestedIf_02.java
@@ -0,0 +1,10 @@
+public class NestedIf_02 {
+    public static void main(String[] args) {
+        int age = 5;
+        if (age >= 10 && age < 20){
+            System.out.println("10대");
+        }else{
+            System.out.println("10대 아님");
+        }
+    }
+}
