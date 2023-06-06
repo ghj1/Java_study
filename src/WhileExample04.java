1a33a2e45b10d94f02f2b2e19c2cf03f48fda56d
ghj1 <ghj12222@gmail.com>
2023-06-06 14:24:08 +0900

    cm

---
+++ src/WhileExample04.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class WhileExample04 {
+    public static void main(String[] args) {
+        String password = "abc123";
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("비밀번호를 입력해주세요.");
+            String input = scanner.nextLine();
+
+            if (input.equals(password)){
+                System.out.println("비밀번호가 일치합니다.");
+                break;
+            }
+            System.out.println("비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
